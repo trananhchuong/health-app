796dae1fe2112e71a7aaf64638178f32cb74366d
chuong tran <chuongninolxag@gmail.com>
2023-01-07 15:41:10 +0700

    add styled component and global style

---
+++ src/constants/VariablesStyles.js
@@ -0,0 +1,37 @@
+export const BG_COLOR = "#F7FAFC";
+export const TABLE_BG_COLOR = "#fefefe";
+export const TABLE_BG_COLOR_ACTIVE = "#EDF8FE";
+export const TABLE_BORDER_COLOR = "#edeef2";
+export const ACTIVE_COLOR = "#25be64";
+export const INACTIVE_COLOR = "#dadde4";
+export const NEW_COLOR = "#febf02";
+export const TEXT_COLOR = "#5698DB";
+export const TABLE_HEAD_BG_COLOR = "#e1e8f2";
+export const HOVER_BG_COLOR = "#F7FAFC";
+export const SELECTED_BG_COLOR = "#5698DB";
+export const HOVER_TEXT_COLOR = "#5698DB";
+export const TEXT_SECONDARY_COLOR = "#171922";
+export const TEXT_THIRD_COLOR = "#FFF";
+
+const size = {
+  mobileS: "400px",
+  tablet: "768px",
+  desktop: "991px",
+};
+
+export const device = {
+  mobileXXS: `(max-width: ${size.mobileS})`,
+  minTablet: `(min-width: ${size.tablet})`,
+  desktop: `(max-width: ${size.desktop})`,
+  tabletToDesktop: `(min-width: ${size.tablet}) and (max-width: ${size.desktop})`,
+};
+
+export const titleSize = {
+  L: "2.4rem",
+  M: "1.4rem",
+};
+
+export const titleLineHeight = {
+  L: "33",
+  M: "19",
+};
